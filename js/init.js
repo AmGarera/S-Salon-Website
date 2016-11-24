a91ca8ef5a570a4cdeb1b558973f8b860dfe1384
amgmetallica <gareraanthony@gmail.com>
2016-11-24 01:14:05 -0500

    Added Modals and tested a larger services list. Additionally added more sidebar and a bios section which is pending info from client.

--- js/init.js
+++ js/init.js
@@ -3,6 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('.collapsible').collapsible();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
